b41a043532345e01809ea638937e31a9d993577c
revalinaadelia <144590332+revalinaadelia@users.noreply.github.com>
2024-10-18 08:30:44 +0700

    App.java
    Source code latihan 1.

---
+++ App.java
@@ -0,0 +1,21 @@
+package app;
+class Luar {
+    private String outerVariable = "Variabel luar";
+
+    class Dalam {
+        String innerVariable = "Variabel dalam";
+
+        public void bicara() {
+            System.out.println(innerVariable);
+            System.out.println(outerVariable);
+        }
+    }
+}
+public class App {
+    public static void main(String[] args) throws Exception {
+        Luar l = new Luar();
+        Luar.Dalam d = l.new Dalam();
+
+        d.bicara();
+    }
+}
